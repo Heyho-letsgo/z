eb58c7d6e0c04dba7b06e736d8217e77de622aca
andy <abaudin@gmail.com>
2013-12-17 12:38:25 +0100

    jusqu'à flash message

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,12 +17,14 @@ include ItemsHelper
 	def create
 		@item = Item.new(item_params)
 		@item.save
+		flash.notice = "Item a été '#{@item.title}' créée !! "
 		redirect_to item_path(@item)
 	end
 	
 	def destroy
 		Item.find(params[:id]).destroy
 		redirect_to items_path()
+		flash.notice = "Item a été détruit !! "
 	end
 
 	def edit
@@ -33,8 +35,11 @@ include ItemsHelper
 	def update
 		@item = Item.find(params[:id])
 		@item.update(item_params)
+		flash.notice = "Item mis '#{@item.title}' à jour"
 		redirect_to item_path(@item)
+
 	end
 
 
 end
+
